558f07c537cd1407908c198769e772e99d5d425b
Anna Lauber <lauber.anna@gmail.com>
2021-02-27 00:52:00 +0100

    Fix build error and JUnit Test

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -5,21 +5,17 @@ import hu.bme.mit.train.interfaces.TrainUser;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
-    TrainSensorImpl tsi;
-
     @Before
     public void before() {
-        tsi = new TrainSensorImpl();
     }
 
     @Test
     public void test() {
-        tsi.overrideSpeedLimit(20);
-        assertEquals(20, tsi.getSpeedLimit());
     }
 }
--- train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
+++ train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
@@ -50,5 +50,11 @@ public class TrainSystemTest {
 		Assert.assertEquals(0, controller.getReferenceSpeed());
 	}
 
+	@Test
+	public void test() {
+		sensor.overrideSpeedLimit(24);
+		Assert.assertEquals(24, sensor.getSpeedLimit());
+	}
+
 	
 }
